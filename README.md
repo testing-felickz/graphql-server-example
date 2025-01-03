e69dc3ce7223568278f494495c1d249117b696f5
Chad Bentz <1760475+felickz@users.noreply.github.com>
2025-01-03 11:16:02 -0500

    samples

--- README.md
+++ README.md
@@ -9,4 +9,54 @@ Tutorial: https://www.apollographql.com/docs/apollo-server/getting-started
 npm start
 ```
 
+visit: http://localhost:4000/
 
+## Sample data
+
+```graphql
+query GetUser($id: ID!) {
+  user(id: $id) {
+    id
+    name
+  }
+  books {
+    title
+    author
+  }
+
+  numberSeven
+  numberSix
+}
+```
+
+Variables
+```json
+{
+  "id": "1"
+}
+```
+
+Response
+
+```json
+{
+  "data": {
+    "user": {
+      "id": "1",
+      "name": "Elizabeth Bennet"
+    },
+    "books": [
+      {
+        "title": "The Awakening",
+        "author": "Kate Chopin"
+      },
+      {
+        "title": "City of Glass",
+        "author": "Paul Auster"
+      }
+    ],
+    "numberSeven": 7,
+    "numberSix": 6
+  }
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,113 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
+// #region const
+// For the purposes of this tutorial, we'll hardcode our example data.
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+
+//For this contrived example, assume our server defines the following hardcoded array:
+const users = [
+    {
+        id: '1',
+        name: 'Elizabeth Bennet',
+    },
+    {
+        id: '2',
+        name: 'Fitzwilliam Darcy',
+    },
+];
+// #endregion
+
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
     author: String
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
+  type User {
+    id: ID
+    name: String
+  }
+
   type Query {
     books: [Book]
+    numberSix: Int!
+    numberSeven: Int!
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    downloadFiles(files: [FileInput]!): Boolean
+  }
+
+  input FileInput {
+    url: String!
   }
 `;
 
-// For the purposes of this tutorial, we'll hardcode our example data.
-const books = [
-    {
-      title: 'The Awakening',
-      author: 'Kate Chopin',
-    },
-    {
-      title: 'City of Glass',
-      author: 'Paul Auster',
-    },
-  ];
-
-
-
-  // Resolvers define how to fetch the types defined in your schema.
+// Resolvers define how to fetch the types defined in your schema.
 // This resolver retrieves books from the "books" array above.
 const resolvers = {
     Query: {
-      books: () => books,
+        books: () => books,
+        numberSix() {
+            return 6;
+        },
+        numberSeven() {
+            return 7;
+        },
+        user(parent, args, contextValue, info) {
+            return users.find((user) => user.id === args.id);
+        },
     },
-  };
+    Mutation: {
+        downloadFiles: async (parent, { files }, context) => {
+            files.forEach((file) => {
+                console.log(`Fetching file: ${file.url}`);
+                // Simulate file fetching
+            });
+            return true;
+        },
+    },
+};
 
+// // OUR SAMPLE
+// // const downloadFiles = createResolver<MutationDownloadFilesArgs, bool>({
+// //     resolver: async ({ args: { files }, ctx }) => {
+// //         files.forEach((file) => {
+// //             logger.debug`Fetchin file`({
+// //                 traceId: ctx.traceId,
+// //             });
+// //             fetch(file.url);
+// //         });
+// //         return true;
+// //     });
 
-  // The ApolloServer constructor requires two parameters: your schema
+// The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-  });
-  
-  // Passing an ApolloServer instance to the `startStandaloneServer` function:
-  //  1. creates an Express app
-  //  2. installs your ApolloServer instance as middleware
-  //  3. prepares your app to handle incoming requests
-  const { url } = await startStandaloneServer(server, {
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
-  });
-  
-  console.log(`🚀  Server ready at: ${url}`);
+});
+
+console.log(`🚀  Server ready at: ${url}`);
